8e050a99a64a4bfa9ffc4732c3a71b7a2ef21056
TimColburn <tim_colburn@yahoo.com>
2020-07-04 11:37:42 -0700

    Add project files.

---
+++ PracticeAutoFac.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeAutoFac;
+using PracticeAutoFac.Controllers;
+using PracticeAutoFac.Models;
+
+namespace PracticeAutoFac.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var person = new Person() { Name = "Test Person" };
+            HomeController controller = new HomeController(person);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
---
+++ PracticeAutoFac/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using PracticeAutoFac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeAutoFac.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Person person;
+
+        // See: https://www.youtube.com/watch?v=lucjChMhTWA
+        // Also, notice I added a unit test for this.
+        public HomeController(Person person)
+        {
+            this.person = person;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
